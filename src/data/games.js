b9e197a3113fe281f4db81ee2ad7551fb4bd6502
dipesh korat <dipeshkorat1998@gmail.com>
2026-05-02 15:11:43 +0200

    Setup GitHub Pages deployment

--- src/data/games.js
+++ src/data/games.js
@@ -11,16 +11,16 @@ export const games = [
       glow: 'shadow-purple-500/50',
       text: 'text-purple-400'
     },
-    icon: '/games/Lumora/App_icon.png',
-    banner: '/games/Lumora/Banner.png',
+    icon: 'games/Lumora/App_icon.png',
+    banner: 'games/Lumora/Banner.png',
     screenshots: [
-      '/games/Lumora/Lumora_ Crystal Saga/1.png',
-      '/games/Lumora/Lumora_ Crystal Saga/2.png',
-      '/games/Lumora/Lumora_ Crystal Saga/3.png',
-      '/games/Lumora/Lumora_ Crystal Saga/4.png',
-      '/games/Lumora/Lumora_ Crystal Saga/5.png',
-      '/games/Lumora/Lumora_ Crystal Saga/6.png',
-      '/games/Lumora/Lumora_ Crystal Saga/7.png'
+      'games/Lumora/Lumora_ Crystal Saga/1.png',
+      'games/Lumora/Lumora_ Crystal Saga/2.png',
+      'games/Lumora/Lumora_ Crystal Saga/3.png',
+      'games/Lumora/Lumora_ Crystal Saga/4.png',
+      'games/Lumora/Lumora_ Crystal Saga/5.png',
+      'games/Lumora/Lumora_ Crystal Saga/6.png',
+      'games/Lumora/Lumora_ Crystal Saga/7.png'
     ],
     features: [
       'Epic Crystal Battles',
@@ -46,14 +46,14 @@ export const games = [
       glow: 'shadow-blue-500/50',
       text: 'text-blue-400'
     },
-    icon: '/games/Block Blaster - Puzzle Game/App_icon.png',
-    banner: '/games/Block Blaster - Puzzle Game/Banner.png',
+    icon: 'games/Block Blaster - Puzzle Game/App_icon.png',
+    banner: 'games/Block Blaster - Puzzle Game/Banner.png',
     screenshots: [
-      '/games/Block Blaster - Puzzle Game/1.png',
-      '/games/Block Blaster - Puzzle Game/2.png',
-      '/games/Block Blaster - Puzzle Game/3.png',
-      '/games/Block Blaster - Puzzle Game/4.png',
-      '/games/Block Blaster - Puzzle Game/5.png'
+      'games/Block Blaster - Puzzle Game/1.png',
+      'games/Block Blaster - Puzzle Game/2.png',
+      'games/Block Blaster - Puzzle Game/3.png',
+      'games/Block Blaster - Puzzle Game/4.png',
+      'games/Block Blaster - Puzzle Game/5.png'
     ],
     features: [
       'Classic, Timed, Zen & Blaster Modes',
@@ -81,16 +81,16 @@ export const games = [
       glow: 'shadow-yellow-500/50',
       text: 'text-yellow-400'
     },
-    icon: '/games/Bolt Master/Bolt Buddy Quest/App_icon.png',
-    banner: '/games/Bolt Master/Bolt Buddy Quest/Banner.png',
+    icon: 'games/Bolt Master/Bolt Buddy Quest/App_icon.png',
+    banner: 'games/Bolt Master/Bolt Buddy Quest/Banner.png',
     screenshots: [
-      '/games/Bolt Master/Bolt Buddy Quest/1.png',
-      '/games/Bolt Master/Bolt Buddy Quest/2.png',
-      '/games/Bolt Master/Bolt Buddy Quest/3.png',
-      '/games/Bolt Master/Bolt Buddy Quest/4.png',
-      '/games/Bolt Master/Bolt Buddy Quest/5.png',
-      '/games/Bolt Master/Bolt Buddy Quest/6.png',
-      '/games/Bolt Master/Bolt Buddy Quest/7.png'
+      'games/Bolt Master/Bolt Buddy Quest/1.png',
+      'games/Bolt Master/Bolt Buddy Quest/2.png',
+      'games/Bolt Master/Bolt Buddy Quest/3.png',
+      'games/Bolt Master/Bolt Buddy Quest/4.png',
+      'games/Bolt Master/Bolt Buddy Quest/5.png',
+      'games/Bolt Master/Bolt Buddy Quest/6.png',
+      'games/Bolt Master/Bolt Buddy Quest/7.png'
     ],
     features: [
       'Mechanical Physics',
@@ -116,14 +116,14 @@ export const games = [
       glow: 'shadow-pink-500/50',
       text: 'text-pink-400'
     },
-    icon: '/games/Fruit Block/Fruit Blast _ Fectory/App_icon.png',
-    banner: '/games/Fruit Block/Fruit Blast _ Fectory/Banner.png',
+    icon: 'games/Fruit Block/Fruit Blast _ Fectory/App_icon.png',
+    banner: 'games/Fruit Block/Fruit Blast _ Fectory/Banner.png',
     screenshots: [
-      '/games/Fruit Block/Fruit Blast _ Fectory/1.png',
-      '/games/Fruit Block/Fruit Blast _ Fectory/2.png',
-      '/games/Fruit Block/Fruit Blast _ Fectory/3.png',
-      '/games/Fruit Block/Fruit Blast _ Fectory/4.png',
-      '/games/Fruit Block/Fruit Blast _ Fectory/5.png'
+      'games/Fruit Block/Fruit Blast _ Fectory/1.png',
+      'games/Fruit Block/Fruit Blast _ Fectory/2.png',
+      'games/Fruit Block/Fruit Blast _ Fectory/3.png',
+      'games/Fruit Block/Fruit Blast _ Fectory/4.png',
+      'games/Fruit Block/Fruit Blast _ Fectory/5.png'
     ],
     features: [
       'Juicy Explosions',
